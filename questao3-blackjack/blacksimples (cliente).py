bea85d0a0b1c0fc81d6410137e47920db63cfe59
gabsfrmarqs <gabrielfrmarques@outlook.com>
2023-10-02 17:26:09 -0300

    eu vou litearalente me matar

--- questao3-blackjack/blacksimples (cliente).py
+++ questao3-blackjack/blacksimples (cliente).py
@@ -1,211 +1,30 @@
-import random
 import socket
-import pickle
 
-# Define the deck of cards
-suits = ['Copas', 'Ouro', 'Paus', 'Espadas']
-ranks = [
-    'Dois', 'Tres', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
-    'Valete', 'Dama', 'Rei', 'As'
-]
+def main():
+    host = '192.168.100.103'
+    porta = 12343
 
-# Create a dictionary to represent card values
-card_values = {
-    'Dois': 2,
-    'Tres': 3,
-    'Quatro': 4,
-    'Cinco': 5,
-    'Seis': 6,
-    'Sete': 7,
-    'Oito': 8,
-    'Nove': 9,
-    'Dez': 10,
-    'Valete': 10,
-    'Dama': 10,
-    'Rei': 10,
-    'As': 11
-}
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((host, porta))
+    print(f"Conectado a {host}:{porta}")
 
+    try:
+        while True:
+            mensagem = cliente.recv(1024).decode()
+            print(mensagem)
 
-# Function to initialize and shuffle the deck
-def initialize_deck():
-  deck = [{'Rank': rank, 'Suit': suit} for rank in ranks for suit in suits]
-  random.shuffle(deck)
-  return deck
+            origem = input("Digite a coordenada da peça a ser movida (linha,coluna): ")
+            destino = input("Digite a coordenada de destino (linha,coluna): ")
 
+            cliente.sendall(origem.encode())
+            cliente.sendall(destino.encode())
 
-# Function to calculate the total value of a hand
-def calculate_hand_value(hand):
-  value = sum(card_values[card['Rank']] for card in hand)
-  # Adjust for Aces
-  for card in hand:
-    if card['Rank'] == 'As' and value > 21:
-      value -= 10
-  return value
+            resposta = cliente.recv(1024).decode()
+            print(resposta)
 
+    except KeyboardInterrupt:
+        print("Conexão encerrada.")
+        cliente.close()
 
-# Function to display a hand
-def display_hand(hand):
-  for card in hand:
-    print(f"{card['Rank']} de {card['Suit']}")
-
-def sum_hand(hand):
-    total_value = 0
-    num_aces = 0  # Count the number of Aces in the hand
-
-    for card in hand:
-        rank = card['Rank']
-        total_value += card_values[rank]
-
-        if rank == 'As':
-            num_aces += 1
-
-    # Adjust for Aces if the total value exceeds 21
-    while num_aces > 0 and total_value > 21:
-        total_value -= 10  # Deduct 10 for each Ace (changes Ace's value from 11 to 1)
-        num_aces -= 1
-
-    return total_value
-
-#FUNÇÃO MAIN
-
-TCP_IP = '192.168.100.103'
-TCP_PORTA = 42107
-TAMANHO_BUFFER = 1024
-
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((TCP_IP, TCP_PORTA))
-
-print(
-    "Bem vindo ao Blackjack (21) via TCP. Você é o cliente, agindo como player."
-)
-print(
-    "Quando um comando for solicitado, digite 'hit' para pedir mais uma carta ou 'stand' para finalizar sua jogada."
-)
-
-player_name = input("Insira seu nome de jogador: ")
-cliente.send(pickle.dumps(player_name))
-
-#received_data = cliente.recv(1024)  # Adjust the buffer size as needed
-#received_tuple = pickle.loads(received_data)
-#print("Received tuple:", received_tuple)
-
-deck = initialize_deck()
-
-# Deal initial cards to the player and dealer
-# Receive the serialized data
-data_received = cliente.recv(TAMANHO_BUFFER)
-
-# Unpickle the entire data
-received_data = pickle.loads(data_received)
-print(received_data)
-
-# Access player_hand and dealer_hand
-player_hand = received_data['player_hand']
-dealer_hand = received_data['dealer_hand']
-
-
-
-
-print("Mão do jogador:")
-display_hand(player_hand)
-
-#print("\nMão do dealer:")
-#print(f"Face up card: \n{dealer_hand[0]['Rank']} de {dealer_hand[0]['Suit']}")
-
-while True: #será usado também para conexão e recebimento de mensagens
-  player_value = calculate_hand_value(player_hand)
-  
-  if player_value == 21:
-  
-    print("\nMão do Player:")
-    display_hand(player_hand)
-    print(f"Soma {player_name}: {sum_hand(player_hand)}")
-    
-    print("\nMão do Dealer:")
-    display_hand(dealer_hand)
-    print(f"Soma dealer: {sum_hand(dealer_hand)}")
-    print(f"Blackjack! {player_name} venceu!")
-    break
-  elif player_value > 21:
-    print("\nMão do Player:")
-    display_hand(player_hand)
-    print(f"Soma {player_name}: {sum_hand(player_hand)}")
-    
-    print("\nMão do Dealer:")
-    display_hand(dealer_hand)
-    print(f"Soma dealer: {sum_hand(dealer_hand)}")
-    print(f"Estouro! {player_name} perdeu.")
-    break
-
- 
-  action = input("\n'hit', 'stand' ou 'chat'? ").lower()
-  if action == 'chat':
-    message = input("Sua mensagem: ")
-    envio = (action,message)
-    cliente.send(pickle.dumps(envio))
-    
-    data = cliente.recv(TAMANHO_BUFFER)
-    received_data = pickle.loads(data)
-
-    #Access player_hand and dealer_hand
-    player_hand = received_data['player_hand']
-    dealer_hand = received_data['dealer_hand']
-  
-  elif action == 'hit':
-    envio = (action,None)
-    cliente.send(pickle.dumps(envio))
-    
-    
-    data = cliente.recv(TAMANHO_BUFFER)
-    received_data = pickle.loads(data)
-
-    #Access player_hand and dealer_hand
-    player_hand = received_data['player_hand']
-    dealer_hand = received_data['dealer_hand']
-    
-    
-    print("\nMão do Player:")
-    display_hand(player_hand)
-    print(f"Soma: {sum_hand(player_hand)}")
-    
-  
-  elif action == 'stand':
-    envio = (action,None)
-    cliente.send(pickle.dumps(envio))
-    dealer_playing = True
-    while dealer_playing == True:
-        data = cliente.recv(TAMANHO_BUFFER)
-        dealer_action = pickle.loads(data)
-        if dealer_action[0] == "chat":
-            print(f"Mensagem do dealer: {dealer_action[1]}")
-        elif dealer_action[0] == "hit":
-            print(f"Dealer usou 'hit'")
-            print(f"+{dealer_action[1]['Rank']} de {dealer_action[1]['Suit']}")
-        elif dealer_action[0] == "stand":
-            print(f"Dealer usou 'stand'!")
-            dealer_playing = False
-    
-    data = cliente.recv(TAMANHO_BUFFER)
-    received_data = pickle.loads(data)
-
-    #Access player_hand and dealer_hand
-    player_hand = received_data['player_hand']
-    dealer_hand = received_data['dealer_hand']
-            
-  else:
-    print("Inválido. Digite 'hit', 'stand' ou 'chat'.")
-
-  dealer_value = calculate_hand_value(dealer_hand)
-  
-
-  if dealer_value > 21:
-    print("Dealer estourou! Player venceu!")
-  elif dealer_value > sum_hand(player_hand):
-    print("Dealer ganhou.")
-  elif dealer_value < sum_hand(player_hand):
-    print("Você ganhou!")
-  else:
-    print("Empate!")
-
-  break
+if __name__ == "__main__":
+    main()
--- questao3-blackjack/blacksimples.py
+++ questao3-blackjack/blacksimples.py
@@ -1,287 +1,87 @@
-import random
 import socket
-import pickle
 
-# Define the deck of cards
-suits = ['Copas', 'Ouro', 'Paus', 'Espadas']
-ranks = [
-    'Dois', 'Tres', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
-    'Valete', 'Dama', 'Rei', 'As'
-]
+# Inicialização do tabuleiro de Damas
+def inicializar_tabuleiro():
+    tabuleiro = [[' ' for _ in range(8)] for _ in range(8)]
+    for i in range(8):
+        for j in range(8):
+            if (i + j) % 2 == 1:
+                if i < 3:
+                    tabuleiro[i][j] = 'O'
+                elif i > 4:
+                    tabuleiro[i][j] = 'X'
+    return tabuleiro
 
-# Create a dictionary to represent card values
-card_values = {
-    'Dois': 2,
-    'Tres': 3,
-    'Quatro': 4,
-    'Cinco': 5,
-    'Seis': 6,
-    'Sete': 7,
-    'Oito': 8,
-    'Nove': 9,
-    'Dez': 10,
-    'Valete': 10,
-    'Dama': 10,
-    'Rei': 10,
-    'As': 11
-}
+# Função para imprimir o tabuleiro
+def imprimir_tabuleiro(tabuleiro):
+    print("  0 1 2 3 4 5 6 7")
+    for i in range(8):
+        print(i, end=' ')
+        for j in range(8):
+            print(tabuleiro[i][j], end=' ')
+        print()
 
+# Função para verificar se um movimento é válido
+def movimento_valido(tabuleiro, jogador, origem, destino):
+    x1, y1 = origem
+    x2, y2 = destino
 
-# Function to initialize and shuffle the deck
-def initialize_deck():
-  deck = [{'Rank': rank, 'Suit': suit} for rank in ranks for suit in suits]
-  random.shuffle(deck)
-  return deck
+    if tabuleiro[x2][y2] != ' ':
+        return False
 
+    if jogador == 'X':
+        if x2 - x1 == 1 and abs(y2 - y1) == 1:
+            return True
+        elif x2 - x1 == 2 and abs(y2 - y1) == 2 and tabuleiro[(x1 + x2) // 2][(y1 + y2) // 2] == 'O':
+            return True
+    elif jogador == 'O':
+        if x1 - x2 == 1 and abs(y2 - y1) == 1:
+            return True
+        elif x1 - x2 == 2 and abs(y2 - y1) == 2 and tabuleiro[(x1 + x2) // 2][(y1 + y2) // 2] == 'X':
+            return True
 
-# Function to calculate the total value of a hand
-def calculate_hand_value(hand):
-  value = sum(card_values[card['Rank']] for card in hand)
-  # Adjust for Aces
-  for card in hand:
-    if card['Rank'] == 'As' and value > 21:
-      value -= 10
-  return value
+    return False
 
+# Função principal do servidor
+def main():
+    host = '192.168.100.103'
+    porta = 12343
 
-# Function to display a hand
-def display_hand(hand):
-  sum = 0
-  for card in hand:
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    servidor.bind((host, porta))
+    servidor.listen(1)
+    print(f"Servidor ouvindo em {host}:{porta}")
 
-    print(f"{card['Rank']} de {card['Suit']}")
-    
-def sum_hand(hand):
-    total_value = 0
-    num_aces = 0  # Count the number of Aces in the hand
+    jogador_atual = 'X'
+    tabuleiro = inicializar_tabuleiro()
 
-    for card in hand:
-        rank = card['Rank']
-        total_value += card_values[rank]
+    while True:
+        cliente, endereco = servidor.accept()
+        print(f"Jogador {jogador_atual} conectado de {endereco}")
 
-        if rank == 'As':
-            num_aces += 1
+        try:
+            while True:
+                imprimir_tabuleiro(tabuleiro)
+                cliente.sendall(str(tabuleiro).encode())
+                origem = cliente.recv(1024).decode()
+                destino = cliente.recv(1024).decode()
 
-    # Adjust for Aces if the total value exceeds 21
-    while num_aces > 0 and total_value > 21:
-        total_value -= 10  # Deduct 10 for each Ace (changes Ace's value from 11 to 1)
-        num_aces -= 1
+                origem = tuple(map(int, origem.split(',')))
+                destino = tuple(map(int, destino.split(',')))
 
-    return total_value
+                if movimento_valido(tabuleiro, jogador_atual, origem, destino):
+                    tabuleiro[destino[0]][destino[1]] = tabuleiro[origem[0]][origem[1]]
+                    tabuleiro[origem[0]][origem[1]] = ' '
+                    cliente.sendall("Movimento válido".encode())
 
-TCP_IP = '192.168.100.103'
-TCP_PORTA = 42107
-TAMANHO_BUFFER = 1024
+                    # Trocar de jogador
+                    jogador_atual = 'O' if jogador_atual == 'X' else 'X'
+                else:
+                    cliente.sendall("Movimento inválido. Tente novamente.".encode())
 
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-servidor.bind((TCP_IP, TCP_PORTA))
-servidor.listen(1)
+        except ConnectionResetError:
+            print(f"Jogador {jogador_atual} desconectado.")
+            cliente.close()
 
-print(f"Servidor disponível na porta {TCP_PORTA} e escutando.....")
-
-conn, addr = servidor.accept()
-print('Endereço conectado:', addr)
-
-data = conn.recv(TAMANHO_BUFFER)
-received = pickle.loads(data)
-print("Nome do jogador:", received)
-
-player_name = received
-
-print(
-    "Bem vindo ao Blackjack (21) via TCP. Você é o servidor, agindo como dealer."
-)
-print(
-    "Quando um comando for solicitado, digite 'hit' para pedir mais uma carta ou 'stand' para finalizar sua jogada."
-)
-
-deck = initialize_deck()
-
-# Deal initial cards to the player and dealer
-player_hand = [deck.pop(), deck.pop()]
-dealer_hand = [deck.pop(), deck.pop()]
-
-data_to_send = {
-    'player_hand': player_hand,
-    'dealer_hand': dealer_hand
-}
-
-serialized_data = pickle.dumps(data_to_send)
-
-conn.send(serialized_data)
-
-#print("Mão do jogador:")
-#display_hand(player_hand)
-
-print("\nMão do dealer:")
-display_hand(dealer_hand)
-
-while True: #será usado também para conexão e recebimento de mensagens
-  player_value = calculate_hand_value(player_hand)
-  if player_value == 21:
-  
-    print("\nMão do Player:")
-    display_hand(player_hand)
-    print(f"Soma {player_name}: {sum_hand(player_hand)}")
-    
-    print("\nMão do Dealer:")
-    display_hand(dealer_hand)
-    print(f"Soma dealer: {sum_hand(dealer_hand)}")
-    print(f"Blackjack! {player_name} venceu!")
-    break
-  elif player_value > 21:
-    print("\nMão do Player:")
-    display_hand(player_hand)
-    print(f"Soma {player_name}: {sum_hand(player_hand)}")
-    
-    print("\nMão do Dealer:")
-    display_hand(dealer_hand)
-    print(f"Soma dealer: {sum_hand(dealer_hand)}")
-    print(f"Estouro! {player_name} perdeu.")
-    break
-
-##ação do jogador PLAYER
-  data = conn.recv(TAMANHO_BUFFER)
-  player_action = pickle.loads(data)
-
-
-  #action = input("\n'hit', 'stand' ou 'chat'? ").lower()
-  if player_action[0] == 'chat':
-    print(f"Mensagem do {player_name}: {player_action[1]}")
-    data_to_send = {
-    'player_hand': player_hand,
-    'dealer_hand': dealer_hand
-}
-
-    serialized_data = pickle.dumps(data_to_send)
-
-    conn.send(serialized_data)
-    
-  elif player_action[0] == 'hit':
-    carta_poppada = deck.pop()
-    player_hand.append(carta_poppada)
-    print(f"\n{player_name} usou 'hit'!")
-    print(f"+{carta_poppada['Rank']} de {carta_poppada['Suit']}")
-    #print("Mão do Player:") 
-    #display_hand(player_hand)
-    #print(f"Soma: {sum_hand(player_hand)}")
-    data_to_send = {
-    'player_hand': player_hand,
-    'dealer_hand': dealer_hand
-}
-
-    serialized_data = pickle.dumps(data_to_send)
-
-    conn.send(serialized_data)
-    
-  elif player_action[0] == 'stand':
-    dealer_playing = True
-    print(f"\n{player_name} usou 'stand'!")
-    while dealer_playing:
-      dealer_action = input("\n'hit', 'stand' ou 'chat'? ").lower()
-      if dealer_action == "chat":
-        message = input("Sua mensagem: ")
-        envio = (dealer_action,message)
-        conn.send(pickle.dumps(envio))
-        
-      elif dealer_action == "hit":
-        
-        carta_poppada_dealer = deck.pop()
-        dealer_hand.append(carta_poppada_dealer)
-        print(f"\nDealer usou 'hit'!\nMão do Dealer:")
-        display_hand(dealer_hand)
-        print(f"Soma: {sum_hand(dealer_hand)}")
-        envio = (dealer_action,carta_poppada_dealer)
-        #print(f"DEBUG: {carta_poppada}")
-        conn.send(pickle.dumps(envio))
-        
-      elif dealer_action == "stand":
-        envio = (dealer_action,None)
-        conn.send(pickle.dumps(envio))
-        dealer_playing = False
-      
-    dealer_value = calculate_hand_value(dealer_hand)
-
-
-    data_to_send = {
-    'player_hand': player_hand,
-    'dealer_hand': dealer_hand
-}
-
-    serialized_data = pickle.dumps(data_to_send)
-
-    conn.send(serialized_data)
-    
-    """
-    print(f"\n{player_name} usou 'stand'!\nMão do Dealer:")
-    display_hand(dealer_hand)
-    print(f"Soma: {sum_hand(dealer_hand)}")
-    """
-    
-    #display_hand(dealer_hand)
-    
-    if dealer_value > 21:
-            print(f"\nVocê estourou! {player_name} venceu!")
-    elif dealer_value > player_value:
-        
-        print(f"\nDealer ganhou! {player_name} perdeu.")
-    elif dealer_value < player_value:
-        print(f"\n{player_name} ganhou!\nVocê perdeu.")
-    else:
-        print("Empate!")
-    dealer_playing = False
-    
-    
-    
-
-    break
-  else:
-    print("Inválido. Digite 'hit', 'stand' ou 'chat'.")
-
-
-"""
-while True: #será usado também para conexão e recebimento de mensagens
-  player_value = calculate_hand_value(player_hand)
-  if player_value == 21:
-    print("Blackjack! Player venceu!")
-    break
-  elif player_value > 21:
-    print("Estouro! Player perdeu.")
-    break
-
-    data = conn.recv(TAMANHO_BUFFER)
-    action = pickle.loads(data)
-    #print("Jogador tirou:", received)
-
-  action = input("\n'hit', 'stand' ou 'chat'? ").lower()
-  if action == 'chat':
-    message_to_send = input("Sua mensagem: ")
-    conn.send(message_to_send.encode("utf-8"))
-  elif action == 'hit':
-    player_hand.append(deck.pop())
-    print("\nMão do Player:")
-    display_hand(player_hand)
-  elif action == 'stand':
-    dealer_value = calculate_hand_value(dealer_hand)
-    while dealer_value < 17:
-      dealer_hand.append(deck.pop())
-      dealer_value = calculate_hand_value(dealer_hand)
-
-    print("\nMão do Dealer:")
-    display_hand(dealer_hand)
-
-    if dealer_value > 21:
-      print("Dealer estourou! Player venceu!")
-    elif dealer_value > player_value:
-      print("Dealer ganhou.")
-    elif dealer_value < player_value:
-      print("Você ganhou!")
-    else:
-      print("Empate!")
-
-    break
-  else:
-    print("Inválido. Digite 'hit', 'stand' ou 'chat'.")
-
-"""
+if __name__ == "__main__":
+    main()
